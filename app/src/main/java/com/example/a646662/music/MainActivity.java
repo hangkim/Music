a0738250b1166a0840b7e7be96eaf515b818db0f
hang kim <hangthang2001>
2018-03-01 10:18:11 -0600

    commit

--- app/src/main/java/com/example/a646662/music/MainActivity.java
+++ app/src/main/java/com/example/a646662/music/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
                     ;
                     return true;
                 case R.id.navigation_play:
-                    mTextMessage.setText(R.string.title_play);
+                    VideoView videoView = findViewById()
                     return true;
                 case R.id.navigation_playlist:
                     mTextMessage.setText(R.string.title_playlist);
